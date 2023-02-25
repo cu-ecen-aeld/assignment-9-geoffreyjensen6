736b35a9537429e50044268386c508e85c9147e8
Geoffrey Jensen <geoffreyjensen6@gmail.com>
2023-02-24 17:11:29 -0800

    Fixed aesd-assignments.mk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = c5bcaced2f9d67c43dfddfd8b8980746beffd67d
+AESD_ASSIGNMENTS_VERSION = 07ccb68af7c65fef7d6f395c9ae4b2fd2f3c6a34
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -28,8 +28,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/bin/
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin/
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket.c $(TARGET_DIR)/bin/
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/user/bin/
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/bin/
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 
 
 
