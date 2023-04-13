ad0bc7237a50d6eb96609710294fb477da708967
Geoffrey Jensen <geoffreyjensen6@gmail.com>
2023-04-12 20:58:56 -0700

    Assignment 8 - Updated to point to passing Assignment 3 Repo

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = 5099bde981e8ed45c03cd55e58547d45b2897466
+AESD_ASSIGNMENTS_VERSION = c9688f542e5ef1899d9529af08923935b9593c1b
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
